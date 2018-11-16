5742eaf9914ff2fe285d90ef318b3474c271003a
AnthonySMyers <33797691+AnthonySMyers@users.noreply.github.com>
2018-11-15 20:21:10 -0800

    Update A6_GrammarRules.java

--- A6_GrammarRules.java
+++ A6_GrammarRules.java
@@ -1,3 +1,14 @@
+import java.util.LinkedList;
 
 public class A6_GrammarRules {
+	private LinkedList<Node[]> rules;
+	private AllNodes nodes;
+	
+	public A6_GrammarRules(){
+		Node[] rule73 = new Node[3];
+		rule73[0] = nodes.Lval;
+		rule73[1] = nodes.equal;
+		rule73[2] = nodes.Expr;
+		rules.add(rule73);
+	}
 }
