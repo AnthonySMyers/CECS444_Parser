955d7c8d68578594404816bffda19617739f7fc4
AnthonySMyers <33797691+AnthonySMyers@users.noreply.github.com>
2018-11-23 22:28:16 -0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -5,6 +5,8 @@ public class Node {
 	private int index;
 	private String name, token;
 	private LinkedList<Node> collection;
+	private Node momNode;
+	public int currentChild, totalChildren;
 	
 	public Node(Type nt, int ind, String name, String token){
 		type = nt;
@@ -22,5 +24,33 @@ public class Node {
 	
 	public void addChild(Node child){
 		collection.add(child);
+	}
+	
+	public Type getType(){
+		return type;
+	}
+	
+	public int getIndex(){
+		return index;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getToken(){
+		return token;
+	}
+	
+	public LinkedList<Node> getChildren(){
+		return collection;
+	}
+	
+	public Node getMom(){
+		return momNode;
+	}
+	
+	public void setMom(Node newMom){
+		momNode = newMom;
 	}
 }
